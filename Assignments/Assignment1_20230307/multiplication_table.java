b205413aad12508a549ae480c5855e33facd5608
Shwetha <Shwetha.A@bytestrone.com>
2023-03-26 11:52:06 +0530

    first assignment added

---
+++ Assignments/Assignment1_20230307/multiplication_table.java
@@ -0,0 +1,19 @@
+package com.bytes.hello;
+
+import java.util.Scanner;
+/**
+ * print multiplication table
+ * @author ShwethaA(Intern)
+ *
+ */
+public class multiplication_table {
+	public static void main(String[] args) {
+		Scanner myObj = new Scanner(System.in);
+		System.out.println("Enter number limit");
+		int num = myObj.nextInt();
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(num + "*" + i + "=" + num * i);
+		}
+	}
+
+}
