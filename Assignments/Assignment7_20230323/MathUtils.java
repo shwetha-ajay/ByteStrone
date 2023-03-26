438e263049f01642b480cc0c7fa67466c52d27a8
Shwetha <Shwetha.A@bytestrone.com>
2023-03-26 19:05:37 +0530

    assignment7 added

---
+++ Assignments/Assignment7_20230323/MathUtils.java
@@ -0,0 +1,18 @@
+package com.bytes.assignment7;
+
+public class MathUtils {
+	private int x;
+	private int y;
+	
+	 static {
+	        System.out.println("MathUtils class loaded");
+	    }
+
+	
+	
+	public static int Product(int x,int y) {
+		return x * y;
+	}
+	
+
+}
---
+++ Assignments/Assignment7_20230323/Person.java
@@ -0,0 +1,18 @@
+package com.bytes.assignment7;
+
+public class Person {
+	private static int count;
+	static {
+		count=0;
+	}
+	private String name;
+	public Person(String name) {
+		this.name=name;
+		count++;
+	}
+	static int  getCount() {
+		return count;
+		
+	}
+
+}
