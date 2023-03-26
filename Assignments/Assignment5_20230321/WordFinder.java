031a581e5d6493a332e6e63023b4b0dd0cf9bb2c
Shwetha <Shwetha.A@bytestrone.com>
2023-03-26 12:36:48 +0530

    assignment6 added

---
+++ Assignments/Assignment5_20230321/WordFinder.java
@@ -0,0 +1,18 @@
+package com.bytes.assignment4;
+import java.util.Scanner;
+public class WordFinder {
+	public static void main (String[]args) {
+		Scanner Obj=new Scanner(System.in);
+		System.out.println("Enter a statement: ");
+		String sentence=Obj.nextLine();
+		System.out.println("Enter the word: ");
+		String word=Obj.nextLine();
+		if(sentence.contains(word)) {
+			System.out.println(word+" is present in the sentence. And is at position "+sentence.indexOf(word));
+		}else {
+			System.out.println(word+" not present in the sentence");
+		
+		}
+	}
+
+}
