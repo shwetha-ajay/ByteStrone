be1cf2c66fedc9c2785ee9d1328dffc2a1532a44
Shwetha <Shwetha.A@bytestrone.com>
2023-03-26 12:10:10 +0530

    assignment3 added

---
+++ Assignments/Assignment3_20230310/VerticalStarPattern.java
@@ -0,0 +1,19 @@
+package com.bytes.hello;
+/**
+ * to print star in a vertical line
+ * @author ShwethaA(Intern)
+ *
+ */
+public class VerticalStarPattern {
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 5; i++) {
+			System.out.print("* ");
+			System.out.print("\n");
+		}
+
+	}
+
+	}
+
+
