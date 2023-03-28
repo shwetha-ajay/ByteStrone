212e3587c571b207a8adfe8e4fdda3fd46d93ee2
Shwetha <Shwetha.A@bytestrone.com>
2023-03-28 13:04:58 +0530

    Assignment1 added

---
+++ Assignments/Assignment1/src/com/bytes/assignment1/Arithmetic.java
@@ -0,0 +1,29 @@
+package com.bytes.assignment1;
+
+import java.util.Scanner;
+/**
+ * enter 2 numbers and perform arithmetic operations.
+ * @author ShwethaA(Intern)
+ *
+ */
+public class Arithmetic {
+	public static void main(String args[]) {
+		Scanner myObj = new Scanner(System.in);
+		System.out.println("enter first no");
+		int num1 = myObj.nextInt();
+		System.out.println("enter second no");
+		int num2 = myObj.nextInt();
+		int sum = num1 + num2;
+		System.out.println("sum is " + sum);
+		int mult = num1 * num2;
+		System.out.println("product is " + mult);
+		int div = num1 / num2;
+		System.out.println("quotient is " + div);
+		int sub = num1 - num2;
+		System.out.println("difference is " + sub);
+		int remain = num1 % num2;
+		System.out.println("remainder is " + remain);
+
+	}
+
+}
---
+++ Assignments/Assignment1/src/com/bytes/assignment1/Concatenate.java
@@ -0,0 +1,21 @@
+package com.bytes.assignment1;
+
+import java.util.Scanner;
+/**
+ * to concatenate 2 strings.
+ * @author ShwethaA(Intern)
+ *
+ */
+public class Concatenate {
+	public static void main(String[] args) {
+		Scanner myObj = new Scanner(System.in);
+		System.out.println("Enter first string");
+
+		String str1 = myObj.nextLine();
+		System.out.println("enter second string ");
+		String str2 = myObj.nextLine();
+		String str = str1 + " " + str2;
+		System.out.println(str);
+
+	}
+}
